0c38a4c8904ba6eb17c6d5665d2dc600b1f7e997
TheOGZer0 <maartenjeu@gmail.com>
2021-03-25 12:46:32 +0100

    Cleaned up

--- MongodbDAO.py
+++ MongodbDAO.py
@@ -24,19 +24,3 @@ def getDocuments(collectionName, filter = {}):
     Returns de gefilterde collectie als object
         """
     return getCollection(collectionName).find(filter)
-###EXPIRIMENTAL
-
-def retrieve_from_dict(dict: dict, key):
-    """Tries to retrieve a value from a dictionairy with a certain key, and catches the KeyError if it doesn't exist.
-
-    Args:
-        dict: the dictionairy to retrieve the value from.
-        key: the key associated with the desired data.
-
-    Returns:
-        if the key exists in the dictionairy, returns the value associated with the key.
-        if it doesn't exist, returns None"""
-    try:
-        return dict[key]
-    except KeyError:
-        return None
