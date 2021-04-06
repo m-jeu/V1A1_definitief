e5d695f9c457fe243a219e4b8b846d16a8411e91
TheOGZer0 <maartenjeu@gmail.com>
2021-04-07 00:53:28 +0200

    Finish get_attribute_price_information() docstring

--- recommendation_rules/buying_power_recommendation/subsubcat_price_ information.py
+++ recommendation_rules/buying_power_recommendation/subsubcat_price_ information.py
@@ -7,7 +7,10 @@ def get_attribute_price_information(db: PostgresDAO.db, product_attribute: str =
 
     Args:
         db: The PostgreSQL database to query.
-        product_attribute: the product_attribute to get (sub_sub_category by default)."""
+        product_attribute: the product_attribute to get (sub_sub_category by default).
+
+    Returns:
+        a list containing a tuple containing: (product_attribute, price) for every product."""
     query = f"SELECT {product_attribute}, selling_price FROM Products;"
     return db.query(query, expect_return=True)
 
